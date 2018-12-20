0a93951d1c17e1263172f1b78ca17bd3768de318
liuwei <liuwei@zench.com>
2018-12-20 16:40:38 +0800

    打印错误日志

--- collector-rabbitmq-client/src/main/java/com/zdzc/collector/rabbitmq/core/MqSender.java
+++ collector-rabbitmq-client/src/main/java/com/zdzc/collector/rabbitmq/core/MqSender.java
@@ -1,11 +1,17 @@
 package com.zdzc.collector.rabbitmq.core;
 
+import ch.qos.logback.core.encoder.ByteArrayUtil;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.MessageProperties;
+import com.zdzc.collector.common.jenum.DataType;
+import com.zdzc.collector.common.jenum.ProtocolType;
+import com.zdzc.collector.common.jfinal.Config;
 import com.zdzc.collector.common.packet.Message;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.datatype.DatatypeConfigurationException;
 import java.io.IOException;
 
 /**
@@ -19,6 +25,15 @@ public class MqSender {
 
     public static void send(Channel channel, Message message, String queueName){
         try {
+            String type = Config.get("protocol.type");
+            if (StringUtils.equals(type, ProtocolType.JT808.getValue()) &&
+                    (message.getHeader().getMsgType() != DataType.HEARTBEAT.getValue())) {
+                String all = message.getAll();
+                String body = ByteArrayUtil.toHexString(message.getBody());
+                if (!all.contains(body)) {
+                    logger.error("body -> {} not belong to -> {}", body, all);
+                }
+            }
             channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getSendBody());
         } catch (IOException e) {
             logger.error(e.getMessage());
