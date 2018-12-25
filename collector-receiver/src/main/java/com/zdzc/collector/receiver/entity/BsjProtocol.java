17a07abd9d30ebddaff083143ce915b0b3f74698
liuwei <liuwei@zench.com>
2018-12-25 13:58:40 +0800

    添加博实结协议映射类

---
+++ collector-receiver/src/main/java/com/zdzc/collector/receiver/entity/BsjProtocol.java
@@ -0,0 +1,101 @@
+package com.zdzc.collector.receiver.entity;
+
+/**
+ * 博实结协议实体类
+ * @Author liuwei
+ * @Description
+ * @Date 2018/12/25 11:58
+ */
+public class BsjProtocol extends Protocol {
+
+    /**
+     * GPS实时补传
+     */
+    private int gpsFill;
+
+    /**
+     * 电压等级
+     */
+    private int voltageLevel;
+
+    /**
+     * GSM信号强度等级
+     */
+    private int signLevel;
+
+    /**
+     * 车辆状态信息
+     */
+    private int vehicleStatus;
+
+    /**
+     * 报警状态信息
+     */
+    private int alarmStatus;
+
+    /**
+     * 当日里程
+     */
+    private double mile;
+
+    /**
+     * 总里程
+     */
+    private double miles;
+
+    public int getGpsFill() {
+        return gpsFill;
+    }
+
+    public void setGpsFill(int gpsFill) {
+        this.gpsFill = gpsFill;
+    }
+
+    public int getVoltageLevel() {
+        return voltageLevel;
+    }
+
+    public void setVoltageLevel(int voltageLevel) {
+        this.voltageLevel = voltageLevel;
+    }
+
+    public int getSignLevel() {
+        return signLevel;
+    }
+
+    public void setSignLevel(int signLevel) {
+        this.signLevel = signLevel;
+    }
+
+    public int getVehicleStatus() {
+        return vehicleStatus;
+    }
+
+    public void setVehicleStatus(int vehicleStatus) {
+        this.vehicleStatus = vehicleStatus;
+    }
+
+    public int getAlarmStatus() {
+        return alarmStatus;
+    }
+
+    public void setAlarmStatus(int alarmStatus) {
+        this.alarmStatus = alarmStatus;
+    }
+
+    public double getMile() {
+        return mile;
+    }
+
+    public void setMile(double mile) {
+        this.mile = mile;
+    }
+
+    public double getMiles() {
+        return miles;
+    }
+
+    public void setMiles(double miles) {
+        this.miles = miles;
+    }
+}
