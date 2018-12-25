b0ce969394183cd222cb0c4020e8bbca50e9ec15
liuwei <liuwei@zench.com>
2018-12-25 17:36:17 +0800

    批量插入尝试

--- collector-receiver/src/main/java/com/zdzc/collector/receiver/coder/MsgDecoder.java
+++ collector-receiver/src/main/java/com/zdzc/collector/receiver/coder/MsgDecoder.java
@@ -1,24 +1,11 @@
 package com.zdzc.collector.receiver.coder;
 
-import com.zdzc.collector.receiver.druid.Crud;
+import com.zdzc.collector.receiver.entity.Protocol;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MsgDecoder {
-    public static void decode (byte[] data) {
-        BsjMessageDecoder.decode(data);
-        String sql = "select * from gps_main.t_gps_main";
-        ResultSet resultSet = Crud.select(sql);
-        try {
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String deviceCode = resultSet.getString("device_code");
-                int seqNo = resultSet.getInt("trail_seq_no");
-                System.out.println("id -> " + id + ";deviceCode -> " + deviceCode + ";seqNo -> " + seqNo);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static Protocol decode (byte[] data) throws SQLException {
+        return BsjMessageDecoder.decode(data);
     }
 }
