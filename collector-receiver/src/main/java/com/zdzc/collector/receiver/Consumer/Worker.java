d46fa1d885a0f91764d346091ace48cbb329b57e
liuwei <liuwei@zench.com>
2018-12-27 17:52:50 +0800

    定位入库逻辑40%

--- collector-receiver/src/main/java/com/zdzc/collector/receiver/Consumer/Worker.java
+++ collector-receiver/src/main/java/com/zdzc/collector/receiver/Consumer/Worker.java
@@ -3,13 +3,12 @@ package com.zdzc.collector.receiver.Consumer;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DeliverCallback;
 import com.zdzc.collector.receiver.coder.MsgDecoder;
-import com.zdzc.collector.receiver.db.DbConnectionPool;
 import com.zdzc.collector.receiver.entity.BsjProtocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Worker implements Runnable {
@@ -18,26 +17,23 @@ public class Worker implements Runnable {
 
     private String queueName;
 
-    private String sql;
-
     private AtomicInteger count = new AtomicInteger(0);
 
     private static final Logger logger = LoggerFactory.getLogger(Worker.class);
 
-    public Worker (Channel channel, String queueName, String sql) {
+    public Worker (Channel channel, String queueName) {
        this.channel = channel;
        this.queueName = queueName;
-       this.sql = sql;
     }
 
     public void doWork() throws Exception {
-        Connection connection = DbConnectionPool.getConnect();
-        PreparedStatement pst = connection.prepareStatement(sql);
+        ConcurrentHashMap<String, List<BsjProtocol>> mapList = new ConcurrentHashMap<>();
+
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             try {
                 BsjProtocol protocol = MsgDecoder.decode(delivery.getBody());
 //                long remainCount = channel.messageCount(queueName);
-                Boolean flag = MessageConsumer.consume(protocol, count.incrementAndGet(), pst);
+                Boolean flag = MessageConsumer.consume(protocol, count.incrementAndGet(), mapList);
                 if (flag) {
                     System.out.println("ack -> " + count.intValue());
                     logger.debug("{} ack -> {}", queueName, count.intValue());
